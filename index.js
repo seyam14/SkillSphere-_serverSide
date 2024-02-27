6445131f1069b49d74e8aab1b3338193de3c11ec
seyam <mdseyam3215@gmail.com>
2024-02-27 21:47:35 +0600

    vercel deployment

--- index.js
+++ index.js
@@ -10,8 +10,6 @@ const port = process.env.PORT || 5000;
 app.use(cors());
 app.use(express.json());
 
-
-
 const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.izczxgs.mongodb.net/?retryWrites=true&w=majority`;
 
