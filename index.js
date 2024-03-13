3ad901f3bb7168f629bc7e389936b6335af6375d
seyam <mdseyam3215@gmail.com>
2024-03-13 14:36:50 +0600

    make admin and delete user

--- index.js
+++ index.js
@@ -51,6 +51,25 @@ async function run() {
         const result = await userCollection.insertOne(user);
         res.send(result);
     });
+    app.patch('/user/admin/:id', async (req, res) => {
+      const id = req.params.id;
+      const filter = { _id: new ObjectId(id) };
+      const updatedDoc = {
+        $set: {
+          role: 'admin'
+        }
+      }
+      const result = await userCollection.updateOne(filter, updatedDoc);
+      res.send(result);
+    })
+   
+    app.delete('/user/:id',  async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: new ObjectId(id) }
+      const result = await userCollection.deleteOne(query);
+      res.send(result);
+    })
+
 
     // add course
     app.post('/addcourse', async (req, res) => {
